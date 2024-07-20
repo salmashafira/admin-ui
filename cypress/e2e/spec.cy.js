9c0e4b8aebaf78c2352c763a3499fc79f182ca0c
salmashafira <111202113372@mhs.dinus.ac.id>
2024-07-20 21:35:08 +0700

    uas salma

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -12,34 +12,11 @@ describe('template spec', () => {
     cy.get('input#password')
       .should('be.visible')
       .should('have.attr', 'placeholder', 'password')
-      .type('123456').should('have.value', '123456')   
+      .type('123456').should('have.value', '123456')
 
     cy.get('[data-testid="submit"]').click()
 
     cy.get('div.home')
-      .should('be.visible')  
-
-    cy.get('div.sidebar')
-      .should('be.visible')   
-    
-    //sidebar -> categories
-    cy.get('[data-testid="categories"]', {timeout: 10000}).should('be.visible').click()  
-    cy.wait(5000); //tunggu 5 detik setelah masuk ke categories
-    cy.url().should('include', '/categories'); //memastikan jika masuk ke halaman yg benar
-    cy.get('div.mydatatableTitle').contains('CATEGORIES')
-
-    //untuk klik tombol add new
-    cy.get('[data-testid="add-new"]').click()
-    cy.wait(3000) //tunggu selama 3 detik setelah menekan add new
-
-    //menulis dessert pada placeholder yang ada
-    cy.get('input[placeholder="Coffee"]', {timeout: 10000}).should('be.visible')
-      .type('Appetizer')
-      .should('have.value', 'Appetizer')
-
-    //menekan button send 
-    cy.get('button[type="submit"]').click()  
-
-
+      .should('be.visible')
   })
 })
